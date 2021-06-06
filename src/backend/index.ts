fe8f76f7a584ec87507ff30b6c95b12b5bb586eb
Daniel Nevius <dmnevius@ncsu.edu>
2021-06-06 16:33:37 -0400

    Re-wrote server startup with Promises

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,30 +1,30 @@
+/* eslint-disable no-console */
 import dotenv from 'dotenv';
 import path from 'path';
 import Server from './Server';
 
 dotenv.config();
 
-/**
- * Main Server startup process
- */
-async function main() {
-  // TODO: change port for deployment
-  const { env } = process;
-  if (env.authKey === undefined) {
-    throw new Error('No authKey found');
-  } else {
-    const server = new Server(env.authKey, path.join(__dirname, 'media'));
-    // TODO: change to production database
-    await server.connect(
+const { env } = process;
+if (env.authKey === undefined) {
+  throw new Error('No authKey found');
+} else {
+  const server = new Server(env.authKey, path.join(__dirname, 'media'));
+  // TODO: change to production database
+  server
+    .connect(
       env.PGUSER as string,
       env.PGPASSWORD as string,
       env.PGHOST as string,
       env.PGPORT as string,
       env.PGDATABASE as string,
-    );
-    await server.start(env.PORT as string);
-    console.log(`Server started on port ${env.PORT}`);
-  }
+    )
+    .then(() => {
+      server.start(env.PORT as string).then(() => {
+        console.log(`Server started on port ${env.PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
-
-main();
